d3c6f09170e529b40e9dce42e7d55187de7b146b
jagdeeparora86 <jagdeeparora86@gmail.com>
2014-04-10 16:22:24 -0700

    socialNetwork Solution.

---
+++ SocialNetworks/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetworks
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			QuickFind qf = new QuickFind(5);
+
+			int frd1, frd2;
+			frd1 = 1;
+			frd2 = 4;
+			qf.connect(frd1, frd2);
+			if (qf.isOneGrandRoot())
+				Console.Write("All frds were now connected at timestamp equivalence to " + frd1 + ", " + frd2 + "Friendship");
+
+			frd1 = 0; frd2 = 2;
+			qf.connect(frd1, frd2);
+			if (qf.isOneGrandRoot())
+				Console.Write("All frds were now connected at timestamp equivalence to " + frd1 + ", " + frd2 + "Friendship");
+
+			frd1 = 0; frd2 = 3;
+			qf.connect(frd1, frd2);
+			if (qf.isOneGrandRoot())
+				Console.Write("All frds were now connected at timestamp equivalence to " + frd1 + ", " + frd2 + "Friendship");
+
+			frd1 = 2; frd2 = 4;
+			qf.connect(frd1, frd2);
+			if (qf.isOneGrandRoot())
+				Console.Write("All frds were now connected at timestamp equivalence to " + frd1 + ", " + frd2 + "Friendship");
+
+		}
+	}
+}
---
+++ SocialNetworks/QuickFind.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetworks
+{
+	class QuickFind
+	{
+		int[] id;
+		int[] sz;
+		int numberOfRoots;
+
+		//constructor to create the array with value of it's own index
+		public QuickFind(int N)
+		{
+			id = new int[N];
+			sz = new int[N];
+
+			for (int i = 0; i < N; i++)
+			{
+				id[i] = i;
+				sz[i] = 1;
+			}
+
+			numberOfRoots = N;
+		}
+
+		//Method to check if it's connected
+		public bool connected(int p, int q)
+		{
+			return root(p) == root(q);
+		}
+
+		//Method to create roots in the array
+		private int root(int value)
+		{
+			while (value != id[value])
+			{
+				// this is making the value to connect to the grandparent of root so the tree is not too big
+				id[value] = id[id[value]];
+				value = id[value];
+			}
+			return value;
+
+		}
+
+		public bool isOneGrandRoot()
+		{
+			return numberOfRoots == 1;
+		}
+
+		public void connect(int p, int q)
+		{
+			int rootp = root(p);
+			int rootq = root(q);
+
+			if (rootp == rootq)
+				return;
+
+			if (sz[p] <= sz[q])
+			{
+				id[p] = id[q];
+				sz[q] += sz[p];
+				numberOfRoots--;
+			}
+			else
+			{
+				id[q] = id[p];
+				sz[p] += sz[q];
+				numberOfRoots--;
+			}
+
+		}
+
+
+	
+	}
+}
